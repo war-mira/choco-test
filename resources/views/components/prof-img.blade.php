b4c6405b3c31db39fe212c22a67c58e1bbeb7045
yanad87 <you@example.com>
2018-07-16 19:18:03 +0300

    img block

--- resources/views/components/prof-img.blade.php
+++ resources/views/components/prof-img.blade.php
@@ -1,5 +1,5 @@
 <div class="parent_cont">
     <img class="entity-thumb-img__img"
      src="{{ file_exists($src) ? URL::asset($src) :URL::asset('images/no-userpic.gif') }}"
-     style="max-width:{{$width ?? '120px'}}; max-height:{{$height ?? '120px'}}" alt="">
+     style="max-width:{{$width ?? '120px'}}; max-height:{{$height ?? '120px'}}" alt="" />
 </div>
