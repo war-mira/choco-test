ba2f9bbc0c5fdfc4456725af441cca7692ef4966
yanad87 <40798730+yanad87@users.noreply.github.com>
2018-08-18 03:37:33 +0300

    add_days

--- app/Http/Controllers/Admin/DoctorController.php
+++ app/Http/Controllers/Admin/DoctorController.php
@@ -69,6 +69,42 @@ class DoctorController extends Controller
             return $this->update($id, $request);
         $redirectRoute = $request->query('redirect', null);
         $data = $this->processRequestData($request);
+
+        if($data['mond_from'] && $data['mond_to'])
+        {
+            $data['mond'] = serialize(array($data['mond_from'],$data['mond_to']));
+        }
+
+        if($data['tues_from'] && $data['tues_to'])
+        {
+            $data['tues'] = serialize(array($data['tues_from'],$data['tues_to']));
+        }
+
+        if($data['wedn_from'] && $data['wedn_to'])
+        {
+            $data['wedn'] = serialize(array($data['wedn_from'],$data['wedn_to']));
+        }
+
+        if($data['thur_from'] && $data['thur_to'])
+        {
+            $data['thur'] = serialize(array($data['thur_from'],$data['thur_to']));
+        }
+
+        if($data['frid_from'] && $data['frid_to'])
+        {
+            $data['frid'] = serialize(array($data['frid_from'],$data['frid_to']));
+        }
+
+        if($data['satu_from'] && $data['satu_to'])
+        {
+            $data['satu'] = serialize(array($data['satu_from'],$data['satu_to']));
+        }
+
+        if($data['sund_from'] && $data['sund_to'])
+        {
+            $data['sund'] = serialize(array($data['sund_from'],$data['sund_to']));
+        }
+
         $doctor = (new Doctor($data));
         $doctor->save();
 
