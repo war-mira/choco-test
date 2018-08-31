8104af4ef97cc0e25dda307d994f42542e16aae4
USER-PC\User <denis.smertni@yandex.ru>
2018-08-31 21:43:12 +0300

    update medcenter avatar in fullpage

--- resources/views/components/prof-img.blade.php
+++ resources/views/components/prof-img.blade.php
@@ -1,5 +1,5 @@
 <div class="parent_cont">
-    <img class="entity-thumb-img__img profiles__img profiles__img--circle @if($class_min > 0 && $class_min >= 200) @else less200 @endif"
+    <img class="entity-thumb-img__img profiles__img profiles__img--circle @if(isset($class_min))@if($class_min > 0 && $class_min >= 200) @else less200 @endif @endif"
      src="{{ file_exists($src) ? URL::asset($src) :URL::asset('images/no-userpic.gif') }}"
      style="max-width:{{$width ?? '120px'}}; max-height:{{$height ?? '120px'}}" alt="" />
 </div>
