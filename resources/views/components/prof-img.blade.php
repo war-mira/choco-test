226cd6ce7ffd554e6218af38f6be78ed35346040
ChesterLi <el.viento.de.plata@gmail.com>
2018-09-07 10:19:48 +0600

    Returned img photos

--- resources/views/components/prof-img.blade.php
+++ resources/views/components/prof-img.blade.php
@@ -1,4 +1,7 @@
-<div class="parent_cont" style="background-image: url({{file_exists($src) ? URL::asset($src) :URL::asset('images/no-userpic.gif')}})">
+<div class="parent_cont">
+    <img class="entity-thumb-img__img profiles__img profiles__img--circle @if(isset($class_min))@if($class_min > 0 && $class_min >= 200) @else less200 @endif @endif"
+     src="{{ file_exists($src) ? URL::asset($src) :URL::asset('images/no-userpic.gif') }}"
+     style="max-width:{{$width ?? '120px'}}; max-height:{{$height ?? '120px'}}" alt="{{$alt ?? ''}}"/>
 </div>
 @if((isset($top5)))
     <div class="entity-thumb-img__label entity-thumb-img__label_top">ТОП-5</div>
