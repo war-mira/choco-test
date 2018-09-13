780dfcea2e6de5f13739e71e7dc0ec3a5b82fd17
Asset <mamiraimov.aset@gmail.com>
2018-09-13 10:40:35 +0600

    cache medcenter coodinates for a week

--- app/Medcenter.php
+++ app/Medcenter.php
@@ -218,11 +218,14 @@ class Medcenter extends Model implements IReferenceable, ISeoMetadata
             $city = City::find($this->city_id);
             $address = $city->name.' '.$this->sms_address;
 
-        $client = new \GuzzleHttp\Client();
-        $response = $client->get('https://geocode-maps.yandex.ru/1.x/?format=json&geocode='.$address.'');
-        $response = $response->getBody();
-        $response = $response->getContents();
-        $response = json_decode($response, true);
+        $response = \Cache::remember('coordinates_'.$this->id,60*24*7,function() use($address){
+            $client = new \GuzzleHttp\Client();
+            $response = $client->get('https://geocode-maps.yandex.ru/1.x/?format=json&geocode='.$address.'');
+            $response = $response->getBody();
+            $response = $response->getContents();
+            $response = json_decode($response, true);
+            return $response;
+        });
         $firstObject = array_shift($response['response']['GeoObjectCollection']['featureMember']);
         $points = $firstObject['GeoObject']['Point']['pos'];
         $points = str_replace(' ', ', ', $points);
