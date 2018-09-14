024897bdca4750c3d6c859652121857e6a90be49
ChesterLi <el.viento.de.plata@gmail.com>
2018-09-14 15:51:26 +0600

    New yandex code

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -2,6 +2,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="yandex-verification" content="8c812ea2ffbaa038" />
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 
 @if(!empty($meta['robots']))
