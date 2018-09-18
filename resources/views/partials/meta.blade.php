b9829ef98c0d28d0535395c369947be5989a07a1
Asset <mamiraimov.aset@gmail.com>
2018-09-18 14:05:28 +0600

    add paging to title

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -10,9 +10,10 @@
     <meta name="robots" content="{{ $meta['robots']}}" />
 @endif
 
+
 <!--Common metadata /-->
 @if(!empty($meta['title']))
-    <title>{{ $meta['title']}}</title>
+    <title>{{ $meta['title']}} @if(!is_null(request()->get('page')) && (request()->get('page') !== 1)) - Страница №{{request()->get('page')}} @endif</title>
 @else
     <title>iDoctor.kz - Поиск врача в Алматы и Астане, бесплатная запись на прием</title>
 @endif
