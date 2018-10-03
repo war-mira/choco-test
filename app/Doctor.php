4dbc4cc980d483e19b7a1b7e8035a6022e52adfe
Asset <mamiraimov.aset@gmail.com>
2018-10-03 13:32:56 +0600

    add from prod

--- app/Doctor.php
+++ app/Doctor.php
@@ -153,7 +153,7 @@ class Doctor extends Model implements IReferenceable, ISeoMetadata
         '524',
         '495'
     ];
-
+    const SHOW_PHONE = 1;
     public $timestamps = true;
     protected $table = 'doctors';
     protected $fillable = [
@@ -210,7 +210,8 @@ class Doctor extends Model implements IReferenceable, ISeoMetadata
         'timetable',
         'seo_text',
         'partner',
-        'showing_phone'
+        'showing_phone',
+        'show_phone'
 //        'mond',
 //        'tues',
 //        'wedn',
