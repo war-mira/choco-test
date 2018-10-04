233e51fefbc78800110ba3fa1e5f566d47b5082c
User <User@IdocFrontDev>
2018-10-04 10:10:02 +0600

    - fixed email templates

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -1,11 +1,11 @@
 <tr>
     <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-            <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" style="margin: 0 auto; padding: 0; text-align: center; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
-                    <tr>
-                        <td class="content-cell" align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
-                            {{ Illuminate\Mail\Markdown::parse($slot) }}
-                        </td>
-                    </tr>
-            </table>
+        <table class="footer" align="center" width="760" cellpadding="0" cellspacing="0" style="margin: 0 auto; padding: 0; text-align: center; width: 760px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 760px;">
+            <tr>
+                <td class="content-cell" align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
+                    {{ Illuminate\Mail\Markdown::parse($slot) }}
+                </td>
+            </tr>
+        </table>
     </td>
 </tr>
