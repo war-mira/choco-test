91faa0673d915b9f083154ab99d9a0f306bb3346
Asset <mamiraimov.aset@gmail.com>
2018-10-10 11:58:16 +0600

    services init

---
+++ app/ServiceGroup.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceGroup extends Model
+{
+    public $timestamps = false;
+
+}
---
+++ database/migrations/2018_10_10_102516_create_service_groups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServiceGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('alias');
+            $table->integer('active')->default(0);
+            $table->index(['active','alias']);
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_groups');
+    }
+}
