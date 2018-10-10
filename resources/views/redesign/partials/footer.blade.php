9326b1d4c56256a256846dad94b6866b99d370e8
User <User@IdocFrontDev>
2018-10-10 16:39:46 +0600

    - Create a category Questions and Answers in the Basement

--- resources/views/redesign/partials/footer.blade.php
+++ resources/views/redesign/partials/footer.blade.php
@@ -16,6 +16,9 @@
                             <a href="{{ route('library.index') }}">Медицинская библиотека</a>
                         </div>
                         <div class="footer-nav-column__item">
+                            <a href="{{ route('question.list') }}">Вопросы и ответы</a>
+                        </div>
+                        <div class="footer-nav-column__item">
                             <a href="{{ route('illnesses.index') }}">Справочник заболеваний</a>
                         </div>
                         <div class="footer-nav-column__item">
