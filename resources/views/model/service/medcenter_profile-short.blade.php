ea6315f9bc068d555b5df053da641dd0cd2feabb
Asset <mamiraimov.aset@gmail.com>
2018-10-11 15:39:20 +0600

    add data from backend

--- resources/views/model/service/medcenter_profile-short.blade.php
+++ resources/views/model/service/medcenter_profile-short.blade.php
@@ -1,41 +1,40 @@
 <div class="entity-line__img">
     <div class="entity-thumb-img">
         <div class="entity-thumb-img__img-wr">
-            <div class="parent_cont medcenters">
-                <img src="/images/SAkUlyWu3m2cyQdIxIYYDUlZHNCIDLEakxOUXNVX.jpeg" alt="SVS Клиника имени Савинова В.М." class="entity-thumb-img__img profiles__img profiles__img--circle   " style="max-width: 250px; max-height: 200px;">
-            </div>
+            @component('components.medcenters-img',['width'=>$width??'200px','height'=>$height??'200px','class_min'=>$medcenter->checkImageheight()??'0', 'alt'=>$medcenter->name])
+                @slot('src')
+                    {{$medcenter->avatar}}
+                @endslot
+            @endcomponent
         </div>
         <div class="entity-thumb-img__rating-line rating-line">
-            <div class="rating-line__val">5</div>
-            <div class="rating-line__stars">
-                <i aria-hidden="true" class="fa fa-star"></i>
-                <i aria-hidden="true" class="fa fa-star"></i>
-                <i aria-hidden="true" class="fa fa-star"></i>
-                <i aria-hidden="true" class="fa fa-star"></i>
-                <i aria-hidden="true" class="fa fa-star"></i>
-            </div>
+            <div class="rating-line__val">{{$medcenter['rate']}}</div>
+            @component('components.rstars',['rating' => $medcenter['rate'] == 0 ? 0:$medcenter['rate']])
+            @endcomponent
         </div>
     </div>
 </div>
 <div class="entity-line__main">
     <div class="h3 profiles__title">
         <div class="entity-line__name">
-            <a href="#">SVS Клиника имени Савинова В.М.</a>
+            <a href="{{ route('medcenter.item',['medcenter'=>$medcenter->alias, 'city' => $medcenter->city->alias]) }}">{{$medcenter->name}}</a>
         </div>
+
     </div>
     <div class="entity-line__about-text">
         <p>
-            Медицинский центр "Савинова" успешно работает уже на протяжении 12 лет. 
-            В самом начале он стартовал в качестве медицинского центра, предоставляющего обширный перечень разнообразных услуг. 
-            Сегодня это уже крупное медицинское учреждение с главным центром в Алматы. В клинике работают высококвалифицированные специалисты, которые готовы оказать качественную медицинскую помощь каждому пациенту.
+            {!! \Illuminate\Support\Str::words($medcenter['content_lite'],30) !!}
         </p>
     </div>
     <div class="clinic-line__brief">
         <div class="clinic-line__brief-line">
             <div class="clinic-line__brief-item">
-                <div class="clinic-line__brief-name">Адрес: </div>
+                <div class="clinic-line__brief-name">
+                    Адрес:
+                </div>
                 <div class="profiles__desc clinic-line__brief-descr">
-                    <a class="link-dotted" href="#">Алматы</a>, ул. Макатаева, 47 (уг. Нусупбекова)
+                    <a class="link-dotted"
+                       href="{{route('medcenters.list')}}">{{$medcenter['city']->name}}</a>, {{$medcenter['map']}}
                 </div>
             </div>
         </div>
@@ -44,11 +43,11 @@
 <div class="entity-line__additional">
     <div class="service-price__heading">
         Стоимость услуги:
-        <p>3500 тг</p>
+        <p>{{$medcenter->pivot->price}} тг</p>
         <p class="service-booking-btn">
             <a href="#order_med" data-doc-id="" data-dname="" data-status="6" class="appointment-book-big__book-btn btn btn_theme_usual trigger-link popup-with-form">
                 Записаться<span class="hidden-xl"> онлайн</span>
-            </a> 
+            </a>
         </p>
     </div>
 </div>
