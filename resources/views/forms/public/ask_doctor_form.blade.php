a61286e6ba2e2fe7c33bf80e92afe55fa26398a6
ChesterLi <el.viento.de.plata@gmail.com>
2018-10-24 14:27:57 +0600

    Removed fields from ask-doctor form

--- resources/views/forms/public/ask_doctor_form.blade.php
+++ resources/views/forms/public/ask_doctor_form.blade.php
@@ -1,50 +1,8 @@
 <form id="ask-doctor-form" class="show-question-form">
-    
-    <div class="form-row">
-        <div class="form-group col-md-6">
-            <label>Ваш пол *</label>
-            <div id="user-gender" class="form-radio">
-                <label class="styled-checkbox_block" for="ask-gender_m">
-                    <input type="radio" checked="checked" name="user[gender]" id="ask-gender_m" value="0" required>
-                    <span class="checkmark"></span>Мужской
-                </label>
-                <label class="styled-checkbox_block">
-                  <input type="radio" name="user[gender]" id="ask-gender_f" value="1" required>
-                  <span class="checkmark"></span>Женский
-                </label>
-            </div>
-        </div>
-    </div>
-    <div class="form-row">
-        <div class="form-group col-md-6" id="form-year">
-            <label>Год рождения *</label>
-            <select name="user[birthday]" id="ask-user-birthday" required class="form-control js-simple-select">
-                <option>Год рождения *</option>
-                    @foreach(range((int)date('Y'),1900) as $i)
-                <option value="{{$i}}">{{$i}}</option>
-                @endforeach
-            </select>
-        </div>
-    </div>
     <div class="form-row">
         <div class="form-group">
             <label>E-mail *</label>
-            <input type="email" id="ask-user-email" class="" name="user[email]" placeholder="example@example.com">
-        </div>
-        <div class="form-group">
-            <label for="mobile_notify">Телефон</label>
-            <input class="bfh-phone" name="user[phone]" id="ask-user-phone" type="text" placeholder="+7 (XXX) XXX-XX-XX" data-mask="+7 (999) 999-99-99">
-        </div>
-    </div>
-    <div class="form-row">
-        <div class="form-group">
-            <label>Специализация</label>
-            <select name="question[skill_id]" class="form-control js-simple-select">
-                <option value="40">Выберите специализацию</option>
-                @foreach(\App\Skill::getList() as $skill)
-                    <option value="{{$skill['id']}}" {{ isset($selectedSkill) && $selectedSkill->id === $skill['id'] ? 'selected':'' }}>{{$skill['name']}}</option>
-                @endforeach
-            </select>
+            <input type="email" id="ask-user-email" class="" name="user[email]" placeholder="example@example.com" required>
         </div>
     </div>
     <div class="form-group">
--- resources/views/redesign/partials/modals/ask-doctor.blade.php
+++ resources/views/redesign/partials/modals/ask-doctor.blade.php
@@ -8,45 +8,9 @@
         </div>
         <form id="ask-doctor-modal-form" class="show-question-form">
                 <div class="form-row">
-                    <div class="form-group col-md-6" id="ask-form-year">
-                        <select name="user[birthday]" id="user-birthday" required class="form-control js-simple-select">
-                            <option>Год рождения *</option>
-                            @foreach(range((int)date('Y'),1900) as $i)
-                            <option value="{{$i}}">{{$i}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label>Ваш пол *</label>
-                        <div id="user-gender">
-                            <input type="radio" name="user[gender]" id="gender_m" value="0" checked required/>
-                            <label for="gender_m">Мужской</label>
-                            <input type="radio" name="user[gender]" id="gender_f"  value="1" checked required/>
-                            <label for="gender_f">Женский</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="form-row">
                     <div class="form-group">
                         <label>E-mail *</label>
-                        <input type="email" id="user-email" class="" name="user[email]" placeholder="example@example.com">
-                    </div>
-                    <div class="form-group">
-                        <label for="mobile_notify">Телефон</label>
-                        <input class="bfh-phone" name="user[phone]" id="user-phone" type="text" placeholder="+7 (XXX) XXX-XX-XX" data-mask="+7 (999) 999-99-99">
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group">
-                        <label>Специализация</label>
-                        <select name="question[skill_id]" class="form-control js-simple-select">
-                            <option value="40">Выберите специализацию</option>
-                            @foreach(\App\Skill::getList() as $skill)
-                                <option value="{{$skill['id']}}">{{$skill['name']}}</option>
-                            @endforeach
-                        </select>
+                        <input type="email" id="user-email" class="" name="user[email]" placeholder="example@example.com" required>
                     </div>
                 </div>
                 <div class="form-group">
