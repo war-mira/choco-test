fc023a05dbb88225279651e6c27d4072e69b3809
Asset <mamiraimov.aset@gmail.com>
2018-11-05 10:41:28 +0600

    add search snippet

---
+++ resources/views/jsond/search.blade.php
@@ -0,0 +1,13 @@
+
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "WebSite",
+  "url": "https://idoctor.kz/",
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": "https://idoctor.kz/almaty/doctors?q={search_term_string}&sort=rate&order=desc",
+    "query-input": "required name=search_term_string"
+  }
+}
+</script>
